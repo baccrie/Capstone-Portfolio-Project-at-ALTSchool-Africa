c03432e6d746bf7982760046957e7ba51c09c8fc
baccrie <bakarerilwan1@gmail.com>
2024-02-26 08:10:11 +0100

    Removing cache middleware for render testing

--- src/routes/locale.ts
+++ src/routes/locale.ts
@@ -19,9 +19,9 @@ import { getAllR, getAllS, getAllL } from '../middleware/cache';
 router.get('/search', search);
 
 // general endpoints
-router.get('/regions', getAllR, getAllRegions);
-router.get('/states', getAllS, getAllStates);
-router.get('/lgas', getAllL, getAllLgas);
+router.get('/regions', getAllRegions);
+router.get('/states', getAllStates);
+router.get('/lgas', getAllLgas);
 
 //specific endpoints
 router.get('/:region/lgas', lgasInRegion);
